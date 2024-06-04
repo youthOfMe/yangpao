00ddfd057a1d3a903d2690a28a3bef7a04c037f3
youthOfMe <2040690180@qq.com>
2024-06-04 21:03:15 +0800

    feat: 测试使用自定义线程池优化循环Betch批量插入数据库10万条数据 耗时5秒

--- src/test/java/com/niuma/usercenter/service/InsertUsersTest.java
+++ src/test/java/com/niuma/usercenter/service/InsertUsersTest.java
@@ -11,7 +11,10 @@ import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 public class InsertUsersTest {
@@ -85,6 +88,9 @@ public class InsertUsersTest {
         System.out.println(stopWatch.getTotalTimeMillis());
     }
 
+    /**
+     * 使用默认线程池优化代码 异步化 10万条数据 5秒
+     */
     @Test
     public void doThreadPoolInsertUsers() {
         StopWatch stopWatch = new StopWatch();
@@ -123,4 +129,48 @@ public class InsertUsersTest {
         stopWatch.stop();
         System.out.println(stopWatch.getTotalTimeMillis());
     }
+
+    /**
+     * 使用自定义线程池优化代码 异步化 10万条数据 5秒
+     */
+    @Test
+    public void doMyThreadPoolInsertUsers() {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10, 10, 10000, TimeUnit.MINUTES, new ArrayBlockingQueue<>(10000));
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        int j = 0;
+        List<CompletableFuture<Void>> futureList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            List<User> userList = new ArrayList<>();
+            while (true) {
+                j++;
+                User user = new User();
+                user.setUsername("牛马洋");
+                user.setUserAccount("fakeniuma");
+                user.setAvatarUrl("https://chenhai-misty-rain-test.oss-cn-beijing.aliyuncs.com/5ed2d23fd474593a3ee433ab73d84aa.jpg");
+                user.setGender(0);
+                user.setUserPassword("d948e8ce556f1de1a3a824bcd86cfa5a");
+                user.setPhone("123");
+                user.setEmail("123");
+                user.setUserStatus(0);
+                user.setUserRole(0);
+                user.setCreateTime(new Date());
+                user.setUpdateTime(new Date());
+                user.setIsDelete(0);
+                user.setPlanetCode("111111");
+                user.setTags("[\"JAVA\", \"GO\"]");
+                if (j % 10000 == 1) {
+                    break;
+                }
+            }
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+                userService.saveBatch(userList, 10000);
+            }, threadPoolExecutor);
+            futureList.add(future);
+        }
+        CompletableFuture.allOf(futureList.toArray(new CompletableFuture[]{})).join();
+        stopWatch.stop();
+        System.out.println(stopWatch.getTotalTimeMillis());
+    }
+
 }
