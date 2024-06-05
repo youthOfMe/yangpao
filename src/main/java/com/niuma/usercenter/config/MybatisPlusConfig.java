6302cf33a1f7f50ef04f8cd6fa03bbaf3e6f379a
youthOfMe <2040690180@qq.com>
2024-06-05 20:36:52 +0800

    feat: 进行配置MybatisPlus的分页拦截器插件

---
+++ src/main/java/com/niuma/usercenter/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.niuma.usercenter.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+
+public class MybatisPlusConfig {
+
+    /**
+     * 添加分页插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        // 创建MybatisPlus拦截器
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 进行配置插件 但是如果进行配置多个插件, 分页插件需要最后添加
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 如果有多数据源可以不配置具体类型, 否则都建议配上具体 DbType
+        return interceptor;
+    }
+}
