e7102ccaf84fc763554bac4a38980461c94ccc22
youthOfMe <2040690180@qq.com>
2024-06-06 21:51:45 +0800

    feat: 生成UserTeam关系表的相关代码

--- src/main/java/com/niuma/usercenter/service/impl/TeamServiceImpl.java
+++ src/main/java/com/niuma/usercenter/service/impl/TeamServiceImpl.java
@@ -1,7 +1,9 @@
 package com.niuma.usercenter.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.niuma.usercenter.common.ErrorCode;
+import com.niuma.usercenter.enums.TeamStatusEnum;
 import com.niuma.usercenter.exception.BusinessException;
 import com.niuma.usercenter.model.domain.Team;
 import com.niuma.usercenter.model.domain.User;
@@ -10,6 +12,7 @@ import com.niuma.usercenter.mapper.TeamMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Optional;
 
 /**
@@ -50,6 +53,38 @@ public class TeamServiceImpl extends ServiceImpl<TeamMapper, Team>
         }
         // 3.4 status 是否公开 (int) 不默认为 0(公开)
         int status = Optional.ofNullable(team.getStatus()).orElse(0);
+        TeamStatusEnum teamStatusEnum = TeamStatusEnum.getEnumByValue(status);
+        if (teamStatusEnum == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "队伍状态不满足要求");
+        }
+        // 3.5 如果status是加密状态, 一定要有密码, 且密码 <= 32
+        String password = team.getPassword();
+        if (TeamStatusEnum.SECRET.equals(teamStatusEnum)) {
+            if (StringUtils.isBlank(password) || password.length() > 32) {
+                throw new BusinessException(ErrorCode.PARAMS_ERROR, "密码设置不正确");
+            }
+        }
+        // 3.6 超时时间 > 当前时间
+        Date expireTime = team.getExpireTime();
+        if (new Date().after(expireTime)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "超时时间必须晚于当前时间");
+        }
+        // 3.7 校验用户最多创建5个队伍
+        QueryWrapper<Team> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userId", userId);
+        long hasTeamNum = this.count(queryWrapper);
+        if (hasTeamNum >= 5) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户最多创建5支队伍");
+        }
+        // 4. 插入队伍信息到队伍表
+        team.setId(null);
+        team.setUserId(userId);
+        boolean result = this.save(team);
+        Long teamId = team.getId();
+        if (!result || teamId == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "创建队伍失败");
+        }
+        // 5. 输入用户 -> 队伍关系到关系表
 
         return 0;
     }
